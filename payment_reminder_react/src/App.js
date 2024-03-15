93e9985dcbc7f608f151b4401898254ae8d35e74
Elliot Park <elliotpark410@gmail.com>
2024-03-15 11:37:52 -0700

    update styling

--- payment_reminder_react/src/App.js
+++ payment_reminder_react/src/App.js
@@ -1,29 +1,43 @@
+// App.js
 import React from 'react';
 import Header from './Header';
 import StudentInfo from './StudentInfo';
 import LessonNumber from './LessonNumber';
 import LessonHistory from './LessonHistory';
 import Reminder from './Reminder';
+import './App.css'; // Import the CSS file
 
 function App() {
   return (
     <div className="app">
       <Header />
       <div className="content">
-        <div className="grid-container">
-          <div className="grid-item">
-            <StudentInfo />
-          </div>
-          <div className="grid-item">
-            <LessonNumber />
-          </div>
-          <div className="grid-item">
-            <LessonHistory />
-          </div>
-          <div className="grid-item">
-            <Reminder />
-          </div>
-        </div>
+        <table className="table">
+          <thead>
+            <tr className="table-row">
+              <th className="table-header">Student Info</th>
+              <th className="table-header">Lesson Number</th>
+              <th className="table-header">Lesson History</th>
+              <th className="table-header">Reminder</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className="table-row">
+              <td className="table-cell">
+                <StudentInfo />
+              </td>
+              <td className="table-cell">
+                <LessonNumber />
+              </td>
+              <td className="table-cell">
+                <LessonHistory />
+              </td>
+              <td className="table-cell">
+                <Reminder />
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </div>
   );
