db91744390095271da88c47d7b6ea0f301da37ae
Elliot Park <elliotpark410@gmail.com>
2024-03-15 16:56:36 -0700

    continue styling student info column. css is the bane of my existence

--- payment_reminder_react/src/StudentInfo.js
+++ payment_reminder_react/src/StudentInfo.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { host } from './lib/constants'
+import { host } from './lib/constants';
 
 function StudentInfo() {
   const [studentData, setStudentData] = useState(null);
@@ -17,8 +17,16 @@ function StudentInfo() {
       {studentData ? (
         studentData.map((student, index) => (
           <div key={index} className="grid-item">
-            <p>Student Name: {student.student_name}</p>
-            <p>Parent Name: {student.parent_name}</p>
+            <table className="table">
+              <tbody>
+                <tr>
+                  <td className="table-cell">Student Name: {student.student_name}</td>
+                </tr>
+                <tr>
+                  <td className="table-cell">Parent Name: {student.parent_name}</td>
+                </tr>
+              </tbody>
+            </table>
           </div>
         ))
       ) : (
