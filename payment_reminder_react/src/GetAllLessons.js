680c8401eb7a8b3c8d73cffc74afa35c7d71d42e
Elliot Park <elliotpark410@gmail.com>
2024-03-27 06:00:17 -0700

    add delete lesson logic for get all lessons button

--- payment_reminder_react/src/GetAllLessons.js
+++ payment_reminder_react/src/GetAllLessons.js
@@ -3,6 +3,8 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Modal, Button } from 'react-bootstrap';
 import { host } from './lib/constants';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
 function GetAllLessons({ onClose }) {
   const [lessons, setLessons] = useState([]);
@@ -30,6 +32,17 @@ function GetAllLessons({ onClose }) {
     fetchLessons();
   }, []);
 
+  const handleDeleteLesson = async (lessonId) => {
+    try {
+      await axios.delete(`${host}/lesson/${lessonId}`);
+      console.log(`Lesson with ID ${lessonId} deleted successfully`);
+      setLessons(lessons.filter(lesson => lesson.id !== lessonId)); // Remove the deleted lesson from the state
+    } catch (error) {
+      console.error('Error deleting lesson:', error);
+      // Handle error here, if necessary
+    }
+  };
+
   return (
     <Modal
       size="lg"
@@ -50,6 +63,7 @@ function GetAllLessons({ onClose }) {
               <th>Number</th>
               <th>Name</th>
               <th>Date</th>
+              <th></th>
             </tr>
           </thead>
           <tbody>
@@ -58,6 +72,14 @@ function GetAllLessons({ onClose }) {
                 <td>{lesson.lessonNumber}</td>
                 <td>{lesson.student_name}</td>
                 <td>{lesson.formattedDate}</td>
+                <td>
+                  <Button
+                    variant="outline-danger"
+                    onClick={() => handleDeleteLesson(lesson.id)} // Pass the lesson ID to delete directly
+                  >
+                    <FontAwesomeIcon icon={faTrash} />
+                  </Button>
+                </td>
               </tr>
             ))}
           </tbody>
