5197c05e387a7ec11237f61d600c8bdfbaed18d4
Elliot Park <elliotpark410@gmail.com>
2024-04-01 17:00:17 -0700

    add error handling for twilio test script

--- server/scripts/twilio.ts
+++ server/scripts/twilio.ts
@@ -3,7 +3,7 @@ dotenv.config();
 
 // Your AccountSID and Auth Token from console.twilio.com
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const authToken = process.env.TWILIO_AUTH_TOKEN;
+const authToken = process.env.TWILIO_TEST_AUTH_TOKEN;
 const twilio_phone_number = process.env.TWILIO_PHONE_NUMBER;
 
 const client = require('twilio')(accountSid, authToken);
@@ -14,4 +14,5 @@ client.messages
     to: '+19495335643', // Text your number
     from: twilio_phone_number, // From a valid Twilio number
   })
-  .then((message: any) => console.log(message.sid));
+  .then((message: any) => console.log(`Message sent successfully. SID: ${message.sid}`))
+  .catch((error: Error)  => console.error(`Error sending message: ${error.message}`));
