a9df1d2875d240671f8d5a87245d4570f033e365
Elliot Park <elliotpark410@gmail.com>
2024-05-08 19:35:14 -0700

    add table-like styling to student view

--- payment_reminder_react/src/components/Student/GetAllStudents.js
+++ payment_reminder_react/src/components/Student/GetAllStudents.js
@@ -6,6 +6,11 @@ import axios from 'axios';
 import { host } from '../../lib/constants';
 import { formatInTimeZone } from 'date-fns-tz';
 
+const rowStyle = (index) => ({
+  borderBottom: '1px solid lightgray', // Thin, faint divider
+  backgroundColor: index % 2 === 0 ? '#f9f9f9' : '#ffffff', // Alternating background colors
+});
+
 const StudentItem = ({
   index,
   student,
@@ -15,7 +20,7 @@ const StudentItem = ({
   onAddLesson,
   onSendText,
   getLessonCount,
-  onResetLessonCount // callback to fetch data after reset
+  onResetLessonCount,
 }) => {
   // Function to reset lesson count
   const resetLessonCount = async (student) => {
@@ -23,7 +28,7 @@ const StudentItem = ({
       const now = new Date(); // Current local time
       const timeZone = 'America/Los_Angeles';
 
-      // Get date in Pacific Time and format it
+      // Get date in Pacific Time
       const formattedDate = formatInTimeZone(now, timeZone, 'yyyy-MM-dd');
 
       await axios.post(`${host}/lesson/reset`, {
@@ -37,68 +42,68 @@ const StudentItem = ({
       }
     } catch (error) {
       console.error('Error resetting lesson count:', error);
+      throw error
     }
   };
 
   return (
-  <Row className="mt-3">
+    <Row style={rowStyle(index)} className="py-2">
       <Col>
-        <div style={{}}>
           {index + 1}
+      </Col>
+      <Col>
+        <div onClick={() => onEdit(student)} style={{ padding: '10px', cursor: 'pointer' }}>
+          <p>{student.student_name}</p>
+          {/* add inentation for parent */}
+          <p>{student.parent_name}</p>
         </div>
-    </Col>
-    <Col>
-      <div onClick={() => onEdit(student)} style={{ padding: '10px', cursor: 'pointer' }}>
-        <p>Student: {student.student_name}</p>
-        <p className="ml-3">Parent: {student.parent_name}</p>
-      </div>
-    </Col>
-    <Col>
-      <Button variant="outline-success" title="Add lesson" onClick={() => onAddLesson(student)}>
-        <FontAwesomeIcon icon={faPlus} />
-      </Button>
-    </Col>
-    <Col>
-      <div className="d-flex align-items-center">
-        <div>
-          {getLessonCount(student.id) >= student.number_of_lessons_in_subscription - 1 ? (
-            <strong>
-              <span style={{ color: '#007bff' }}>{getLessonCount(student.id)}</span>
-            </strong>
-          ) : (
-            <span>{getLessonCount(student.id)}</span>
-          )}
-          {' '}
-          / {student.number_of_lessons_in_subscription}
+      </Col>
+      <Col>
+        <Button variant="outline-success" title="Add lesson" onClick={() => onAddLesson(student)}>
+          <FontAwesomeIcon icon={faPlus} />
+        </Button>
+      </Col>
+      <Col>
+        <div className="d-flex align-items-center">
+          <div>
+            {getLessonCount(student.id) >= student.number_of_lessons_in_subscription - 1 ? (
+              <strong>
+                <span style={{ color: '#007bff' }}>{getLessonCount(student.id)}</span>
+              </strong>
+            ) : (
+              <span>{getLessonCount(student.id)}</span>
+            )}
+            {' '}
+            / {student.number_of_lessons_in_subscription}
+          </div>
         </div>
-      </div>
-    </Col>
-    <Col>
-      <Button variant="outline-warning" title="Reset lesson count" onClick={() => resetLessonCount(student)}>
-        <FontAwesomeIcon icon={faSyncAlt} />
-      </Button>
-    </Col>
-    <Col>
-      <Button variant="outline-success" title="View lessons" onClick={() => onViewLessons(student)}>
-        View Lessons
-      </Button>
-    </Col>
-    <Col>
-      <Button
-        variant={getLessonCount(student.id) < student.number_of_lessons_in_subscription - 1 ? 'outline-secondary' : 'outline-primary'}
-        title="Send text"
-        onClick={() => onSendText(student)}
-        disabled={getLessonCount(student.id) < student.number_of_lessons_in_subscription - 1}
-      >
-        Send Text
-      </Button>
-    </Col>
-    <Col>
-      <Button variant="outline-danger" title="Delete student" onClick={() => onDelete(student)}>
-        <FontAwesomeIcon icon={faTrash} />
-      </Button>
-    </Col>
-  </Row>
+      </Col>
+      <Col>
+        <Button variant="outline-warning" title="Reset lesson count" onClick={() => resetLessonCount(student)}>
+          <FontAwesomeIcon icon={faSyncAlt} />
+        </Button>
+      </Col>
+      <Col>
+        <Button variant="outline-success" title="View lessons" onClick={() => onViewLessons(student)}>
+          View Lessons
+        </Button>
+      </Col>
+      <Col>
+        <Button
+          variant={getLessonCount(student.id) < student.number_of_lessons_in_subscription - 1 ? 'outline-secondary' : 'outline-primary'}
+          title="Send text"
+          onClick={() => onSendText(student)}
+          disabled={getLessonCount(student.id) < student.number_of_lessons_in_subscription - 1}
+        >
+          Send Text
+        </Button>
+      </Col>
+      <Col>
+        <Button variant="outline-danger" title="Delete student" onClick={() => onDelete(student)}>
+          <FontAwesomeIcon icon={faTrash} />
+        </Button>
+      </Col>
+    </Row>
   );
 };
 
@@ -111,35 +116,18 @@ const GetAllStudents = ({
   getLessonCountForStudent,
   onAddLessonClick,
   onSendTextClick,
-  resetLessonCountForStudentClick
+  resetLessonCountForStudentClick,
 }) => (
   <>
-    {/* Row of column headers */}
-    <Row className="mt-3">
-      <Col>
-        <strong>Number</strong>
-      </Col>
-      <Col>
-        <strong>Name</strong>
-      </Col>
-      <Col>
-        <strong>Add Lesson</strong>
-      </Col>
-      <Col>
-        <strong>Lessons</strong>
-      </Col>
-      <Col>
-        <strong>Reset</strong>
-      </Col>
-      <Col>
-        <strong>View Lessons</strong>
-      </Col>
-      <Col>
-        <strong>Send Text</strong>
-      </Col>
-      <Col>
-        <strong>Delete</strong>
-      </Col>
+    <Row style={{ borderBottom: '1px solid lightgray' }} className="py-2">
+      <Col><strong>Number</strong></Col>
+      <Col><strong>Name</strong></Col>
+      <Col><strong>Add Lesson</strong></Col>
+      <Col><strong>Lessons</strong></Col>
+      <Col><strong>Reset</strong></Col>
+      <Col><strong>View Lessons</strong></Col>
+      <Col><strong>Send Text</strong></Col>
+      <Col><strong>Delete</strong></Col>
     </Row>
     {/* Rows of student items */}
     {students.map((student, index) => (
