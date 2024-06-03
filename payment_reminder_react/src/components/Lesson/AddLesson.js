93854516db8f4aa54e66835dbca0ff96a0507883
Elliot Park <elliotpark410@gmail.com>
2024-06-03 16:06:09 -0700

    update calendar to include tile content for lessons

--- payment_reminder_react/src/components/Lesson/AddLesson.js
+++ payment_reminder_react/src/components/Lesson/AddLesson.js
@@ -3,10 +3,11 @@ import React, { useState, useEffect } from 'react';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { Modal, Button } from 'react-bootstrap';
-import LessonCalendar from './Calendar/Calendar'; // Import your calendar component
-import axios from 'axios'; // Import Axios
+import LessonCalendar from './Calendar/Calendar';
+import axios from 'axios';
 import { host } from '../../lib/constants';
 import AddPayment from '../Payment/AddPayment';
+import { formatDate } from '../../lib/util';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSyncAlt, faBook, faCreditCard } from '@fortawesome/free-solid-svg-icons';
 import '../../App.css';
@@ -18,11 +19,42 @@ function AddLesson({
   onUpdate
  }) {
   const [selectedDate, setSelectedDate] = useState(null);
+  const [lessons, setLessons] = useState([]);
   const [showPaymentModal, setShowPaymentModal] = useState(false);
 
   useEffect(() => {
     setSelectedDate(new Date());
-  }, []);
+    fetchStudentLessons(studentId);
+  }, [studentId]);
+
+  const fetchStudentLessons = async (studentId) => {
+    try {
+      const response = await axios.get(`${host}/lesson/student/${studentId}`);
+
+      // Filter out records with null or invalid lesson_date
+      const validLessons = response.data.filter((lesson) => {
+        return lesson.lesson_date !== null && lesson.lesson_date !== undefined && lesson.deleted_at === null;
+      });
+
+      // Sort the valid records by lesson_date
+      const sortedLessons = validLessons.sort(
+        (a, b) => new Date(a.lesson_date) - new Date(b.lesson_date)
+      );
+
+      // Format the sorted lessons
+      const formattedLessons = sortedLessons.map((lesson, index) => ({
+        ...lesson,
+        lesson: true,
+        lessonNumber: index + 1,
+        formattedDate: formatDate(lesson.lesson_date)
+      }));
+
+      setLessons(formattedLessons);
+    } catch (error) {
+      console.error('Error fetching lessons:', error);
+      throw error;
+    }
+  };
 
   // Function to handle add lesson on the selected date
   const handleAddLesson = async () => {
@@ -46,6 +78,7 @@ function AddLesson({
         toast.success(`Added lesson`, {
           autoClose: 3000, // Close after 3 seconds
         });
+        fetchStudentLessons(studentId);
         onUpdate();
       } else {
         console.error('Error adding lesson. Unexpected response:', response);
@@ -81,6 +114,7 @@ function AddLesson({
         toast.warning(`Lesson reset on ${notificationDate}`, {
           autoClose: 3000, // Close after 3 seconds
         });
+        fetchStudentLessons(studentId);
         onUpdate();
       } else {
         console.error('Error resetting lesson count. Unexpected response:', response);
@@ -111,7 +145,10 @@ function AddLesson({
           <Modal.Title>{studentName}</Modal.Title>
         </Modal.Header>
         <Modal.Body style={{ padding: 0, height: '100%' }}>
-          <LessonCalendar onSelectDate={(date) => setSelectedDate(date)} selectedDate={selectedDate}/>
+          <LessonCalendar
+          onSelectDate={(date) => setSelectedDate(date)} selectedDate={selectedDate}
+          lessons={lessons}
+          />
         </Modal.Body>
         <Modal.Footer>
           <div style={{ flex: 1, textAlign: 'left' }}>
--- payment_reminder_react/src/components/Lesson/Calendar/Calendar.js
+++ payment_reminder_react/src/components/Lesson/Calendar/Calendar.js
@@ -3,12 +3,31 @@ import { Calendar } from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import './calendarStyles.css';
 
-function LessonCalendar({ onSelectDate }) {
+function LessonCalendar({ onSelectDate, lessons }) {
+
+  const tileContent = ({ date, view }) => {
+    if (view === 'month') {
+      const lessonDates = lessons.map(lesson => new Date(lesson.lesson_date).toDateString());
+      if (lessonDates.includes(date.toDateString())) {
+        return <div
+          style={{
+            height: '10px',
+            backgroundColor: 'blue',
+            borderRadius: '5px',
+            marginTop: '10px'
+          }}
+        />;
+      }
+    }
+    return null;
+  };
+
   return (
     <div className="d-flex justify-content-center">
       <Calendar
         onClickDay={(date) => onSelectDate(date)} // Use onClickDay instead of onSelectSlot
         className="shadow-none border-0" // Remove default calendar shadow and border
+        tileContent={tileContent}
       />
     </div>
   );
