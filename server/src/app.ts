68977c4be7f40a847177983267f6198a2407af8c
Elliot Park <elliotpark410@gmail.com>
2024-07-08 09:46:26 -0700

    update cors options

--- server/src/app.ts
+++ server/src/app.ts
@@ -32,7 +32,7 @@ app.use(limiter);
 // Configure CORS
 const corsOptions = {
   origin: environment === 'production'
-    ? [domain, 'https://main.d3ilsjmv4na455.amplifyapp.com/']
+    ? [domain]
     : ['http://localhost:3000'],
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
