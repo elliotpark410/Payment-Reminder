6ed0a010e5ee616f46ecba7b7b007587f179b2f4
Elliot Park <elliotpark410@gmail.com>
2024-07-08 08:57:36 -0700

    update host for api call to use https

--- payment_reminder_react/src/lib/constants.js
+++ payment_reminder_react/src/lib/constants.js
@@ -1,3 +1,3 @@
-export const host = 'http://www.parkvocalstudiomanagement.com/api';
+export const host = 'https://www.parkvocalstudiomanagement.com/api';
 // export const host = 'http://localhost:3000';
 export const website = 'https://parkvocalstudio.com/';
