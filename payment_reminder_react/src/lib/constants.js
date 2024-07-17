01b2908985d6bffc59568816fffa7a30ee2b0669
Elliot Park <elliotpark410@gmail.com>
2024-07-17 15:19:45 -0700

    add env var to fe

--- payment_reminder_react/src/lib/constants.js
+++ payment_reminder_react/src/lib/constants.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
+import dotenv from 'dotenv';
+dotenv.config();
 
-export const host = 'https://api.parkvocalstudiomanagement.com';
-// export const host = 'http://localhost:3000';
+export const host = process.env.API_HOST;
 export const website = 'https://parkvocalstudio.com/';
 export const api = axios.create({
   baseURL: host,
