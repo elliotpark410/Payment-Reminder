b1c77deebf3cc107dc191633a76ba80b504befea
Elliot Park <elliotpark410@gmail.com>
2024-07-17 15:30:21 -0700

    uninstall dotenv

--- payment_reminder_react/src/lib/constants.js
+++ payment_reminder_react/src/lib/constants.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-export const host = process.env.API_HOST;
+export const host = 'https://api.parkvocalstudiomanagement.com';
+// export const host = 'http://localhost:3000';
 export const website = 'https://parkvocalstudio.com/';
 export const api = axios.create({
   baseURL: host,
