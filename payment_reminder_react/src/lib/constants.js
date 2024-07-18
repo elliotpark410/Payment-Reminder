f29b54dd47a0e97290a8b505be58f1deb59f7b24
Elliot Park <elliotpark410@gmail.com>
2024-07-17 18:04:39 -0700

    remove env var to debug performance degradation

--- payment_reminder_react/src/lib/constants.js
+++ payment_reminder_react/src/lib/constants.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
 
-let host = 'http://localhost:3000';
+// let host = 'http://localhost:3000';
+const host = 'https://api.parkvocalstudiomanagement.com';
 
-if (process.env.NODE_ENV === 'production') {
-  host = process.env.REACT_APP_API_HOST
-}
+// if (process.env.NODE_ENV === 'production') {
+//   host = process.env.REACT_APP_API_HOST
+// }
 
 export const api = axios.create({
   baseURL: host,
