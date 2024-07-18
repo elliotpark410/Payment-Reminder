81038f8460751a6bc6bbd866c7ef070b0dc87ae8
Elliot Park <elliotpark410@gmail.com>
2024-07-17 18:11:19 -0700

    undo las commit

--- payment_reminder_react/src/lib/constants.js
+++ payment_reminder_react/src/lib/constants.js
@@ -1,11 +1,10 @@
 import axios from 'axios';
 
-// let host = 'http://localhost:3000';
-const host = 'https://api.parkvocalstudiomanagement.com';
+let host = 'http://localhost:3000';
 
-// if (process.env.NODE_ENV === 'production') {
-//   host = process.env.REACT_APP_API_HOST
-// }
+if (process.env.NODE_ENV === 'production') {
+  host = process.env.REACT_APP_API_HOST
+}
 
 export const api = axios.create({
   baseURL: host,
