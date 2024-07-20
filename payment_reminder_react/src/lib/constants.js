a0672cfc685e3b0e2ec0f46627c7250559ee41fe
Elliot Park <elliotpark410@gmail.com>
2024-07-20 02:57:17 -0700

    update api instance to use bearer token

--- payment_reminder_react/src/lib/constants.js
+++ payment_reminder_react/src/lib/constants.js
@@ -11,5 +11,19 @@ export const api = axios.create({
   withCredentials: true
 });
 
+// Adding a request interceptor to include the authorization token
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token'); // or wherever you store your token
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
 export const website = 'https://parkvocalstudio.com/';
 
